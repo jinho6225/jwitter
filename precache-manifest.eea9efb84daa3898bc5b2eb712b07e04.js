f8737cdfa6d05af7ed309d8ae18d4b678909ab4a
jinho6225 <jinho6225@hotmail.com>
2020-10-30 10:03:52 -0700

    Updates

---
+++ precache-manifest.eea9efb84daa3898bc5b2eb712b07e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e707489d227881d410748b0b5d6115b4",
+    "url": "/jwitter/index.html"
+  },
+  {
+    "revision": "69afacd8e4d1840d8ebf",
+    "url": "/jwitter/static/css/main.2ff3d120.chunk.css"
+  },
+  {
+    "revision": "bcb5df8b5f84d639edfa",
+    "url": "/jwitter/static/js/2.6a80aa49.chunk.js"
+  },
+  {
+    "revision": "7483fa22998193a731cb6c630727fbf9",
+    "url": "/jwitter/static/js/2.6a80aa49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69afacd8e4d1840d8ebf",
+    "url": "/jwitter/static/js/main.5d9be89f.chunk.js"
+  },
+  {
+    "revision": "d27a12203f970ffcd255",
+    "url": "/jwitter/static/js/runtime-main.3f2c118c.js"
+  }
+]);
